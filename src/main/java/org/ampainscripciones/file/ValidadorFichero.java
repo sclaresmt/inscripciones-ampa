28b4f5907c8d2dc91fcd66f2aff097fd29ac0921
Sergio <sclaresmt@gmail.com>
2022-05-18 20:50:10 +0200

    Methods to extract data to validate

--- src/main/java/org/ampainscripciones/file/ValidadorFichero.java
+++ src/main/java/org/ampainscripciones/file/ValidadorFichero.java
@@ -1,6 +1,7 @@
 package org.ampainscripciones.file;
 
 import org.ampainscripciones.model.InscriptionDTO;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.*;
@@ -14,49 +15,42 @@ import java.util.*;
 
 public class ValidadorFichero {
 
-
-    protected Map<Integer, InscriptionDTO> extractInscriptionsData(File file) throws IOException, InvalidFormatException {
+    protected Map<Integer, String> extractEmailData(File file) throws IOException {
 
         if (!file.exists()) {
             throw new IOException(String.format("File %s does not exist!", file.getAbsolutePath()));
         }
 
-        Map<Integer, InscriptionDTO> sheetValuesByRowIndex = new HashMap<>();
-        OPCPackage pkg = OPCPackage.open(file);
-        try (XSSFWorkbook wb = new XSSFWorkbook(pkg)) {
+        Map<Integer, String> emailValuesByRowIndex = new LinkedHashMap<>();
+        try (Workbook wb = WorkbookFactory.create(file)) {
 
-            Sheet sheetAt = wb.getSheetAt(0);
-            for (Iterator<Row> rowIterator = sheetAt.rowIterator(); rowIterator.hasNext();) {
-
-                List<String> rowValues = new ArrayList<>();
-                Row row = rowIterator.next();
-                InscriptionDTO inscriptionDTO = new InscriptionDTO();
-                inscriptionDTO.setTimestamp(LocalDateTime.from(DateTimeFormatter.ofPattern("M/d/y hh:mm:ss")
-                        .parse(row.getCell(0).getStringCellValue())));
-                inscriptionDTO.setEmail(row.getCell(1).getStringCellValue());
-                inscriptionDTO.setParent1Name(row.getCell(2).getStringCellValue());
-                inscriptionDTO.setParent1PhoneNumber(row.getCell(3).getNumericCellValue());
-                inscriptionDTO.setParent2Name(row.getCell(4).getStringCellValue());
-                inscriptionDTO.setParent2PhoneNumber(row.getCell(5).getNumericCellValue());
-                inscriptionDTO.setAusiasChildrenNumber(row.getCell(6).getNumericCellValue());
-                inscriptionDTO.setAusiasChild1Name(row.getCell(7).getStringCellValue());
-                inscriptionDTO.setAusiasChild1Course(row.getCell(8).getStringCellValue());
-                inscriptionDTO.setAusiasChild2Name(row.getCell(9).getStringCellValue());
-                inscriptionDTO.setAusiasChild2Course(row.getCell(10).getStringCellValue());
-                inscriptionDTO.setLluisChildrenNumber(row.getCell(11).getNumericCellValue());
-                inscriptionDTO.setLluisChild1Name(row.getCell(12).getStringCellValue());
-                inscriptionDTO.setLluisChild1Course(row.getCell(13).getStringCellValue());
-                inscriptionDTO.setLluisChild2Name(row.getCell(14).getStringCellValue());
-                inscriptionDTO.setLluisChild2Course(row.getCell(15).getStringCellValue());
-                inscriptionDTO.setPaymentFileUrl(row.getCell(16).getStringCellValue());
-                inscriptionDTO.setProtectionDataPolicy(row.getCell(17).getStringCellValue());
-                inscriptionDTO.setChildrenImageAuthorization(row.getCell(18).getStringCellValue());
-                sheetValuesByRowIndex.put(row.getRowNum(), inscriptionDTO);
+            Sheet sheet = wb.getSheetAt(0);
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                emailValuesByRowIndex.put(i, sheet.getRow(i).getCell(1).getStringCellValue());
             }
 
         }
 
-        return sheetValuesByRowIndex;
+        return emailValuesByRowIndex;
+    }
+
+    protected List<String> extractPaymentsData(File file) throws IOException {
+        if (!file.exists()) {
+            throw new IOException(String.format("File %s does not exist!", file.getAbsolutePath()));
+        }
+
+        List<String> paymentDescription = new ArrayList<>();
+        try (Workbook wb = WorkbookFactory.create(file)) {
+            Sheet sheet = wb.getSheetAt(0);
+            for (int i = 3; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+                double numericCellValue = row.getCell(4).getNumericCellValue();
+                if (numericCellValue == 15.00D) {
+                    paymentDescription.add(row.getCell(3).getStringCellValue());
+                }
+            }
+        }
+        return paymentDescription;
     }
 
 }
--- src/test/java/org/ampainscripciones/file/ValidadorFicheroTest.java
+++ src/test/java/org/ampainscripciones/file/ValidadorFicheroTest.java
@@ -1,6 +1,5 @@
 package org.ampainscripciones.file;
 
-import org.ampainscripciones.model.InscriptionDTO;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,28 +17,64 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class ValidadorFicheroTest {
 
-    private static String INSCRIPTIONS_TEST_FILE = "./src/test/resources/inscriptions_test.xlsx";
+    private static final String INSCRIPTIONS_TEST_FILE = "./src/test/resources/inscriptions_test.xlsx";
+
+    private static final String PAYMENTS_TEST_FILE = "./src/test/resources/Movimientos_cuenta_0281573.xls";
 
     @Spy
     @InjectMocks
     private ValidadorFichero validadorFichero;
 
     @Test
-    public void extractInscriptionsData() throws IOException, InvalidFormatException {
+    public void extractEmailData() throws IOException, InvalidFormatException {
         File file = new File(INSCRIPTIONS_TEST_FILE);
 
-        Map<Integer, InscriptionDTO> data = this.validadorFichero.extractInscriptionsData(file);
+        Map<Integer, String> data = this.validadorFichero.extractEmailData(file);
 
-        assertEquals(9, data.size());
-        assertEquals("pepitopalotes33@gmail.com", data.get(0).getEmail());
-        assertEquals("pepitopalotes34@gmail.com", data.get(1).getEmail());
-        assertEquals("pepitopalotes35@gmail.com", data.get(2).getEmail());
-        assertEquals("pepitopalotes36@gmail.com", data.get(3).getEmail());
-        assertEquals("pepitopalotes37@gmail.com", data.get(4).getEmail());
-        assertEquals("pepitopalotes38@gmail.com", data.get(5).getEmail());
-        assertEquals("lafigatatia@gmail.com", data.get(6).getEmail());
-        assertEquals("lamarequeva@gmail.com", data.get(7).getEmail());
-        assertEquals("latiatamare@gmail.com", data.get(8).getEmail());
+        assertEquals(10, data.size());
+        assertEquals("pepitopalotes@gmail.com", data.get(1));
+        assertEquals("pepitopalotes@gmail.com", data.get(2));
+        assertEquals("pepitopalotes34@gmail.com", data.get(3));
+        assertEquals("pepitopalotes35@gmail.com", data.get(4));
+        assertEquals("pepitopalotes36@gmail.com", data.get(5));
+        assertEquals("pepitopalotes37@gmail.com", data.get(6));
+        assertEquals("pepitopalotes38@gmail.com", data.get(7));
+        assertEquals("lafigatatia@gmail.com", data.get(8));
+        assertEquals("lamarequeva@gmail.com", data.get(9));
+        assertEquals("latiatamare@gmail.com", data.get(10));
+    }
+
+    @Test
+    public void extractEmailDataThrowsIOExceptionWhenFileDoesNotExist() {
+        File file = new File("A non existing file");
+
+        assertThrows(IOException.class, () -> this.validadorFichero.extractEmailData(file));
+    }
+
+    @Test
+    public void extractPaymentsData() throws IOException, InvalidFormatException {
+        File file = new File(PAYMENTS_TEST_FILE);
+
+        List<String> data = this.validadorFichero.extractPaymentsData(file);
+
+        assertEquals(10, data.size());
+        assertEquals("XXXXXXXX-pepitopalotes@gmail.com", data.get(0));
+        assertEquals("XXXXXXXX-PAULA APELLIDO APELLIDO2", data.get(1));
+        assertEquals("CCCXXXXXXXXXXXXXXXXXXXX", data.get(2));
+        assertEquals("YYYYYYYY-LINA APELLIDO APELLIDO", data.get(3));
+        assertEquals("ZZZZZZZZ-LAURA LAURA LAURA", data.get(4));
+        assertEquals("AAAAAAAA-INMACULADA INMA INMA", data.get(5));
+        assertEquals("XXXXXXXX-pepitopalotes34@gmail.com", data.get(6));
+        assertEquals("XXXXXXXX-pepitopalotes35@gml.com", data.get(7));
+        assertEquals("XXXXXXXX-lamarequeva@gmail.com", data.get(8));
+        assertEquals("XXXXXXXX-pepitopalotes36@hotmail.com", data.get(9));
+    }
+
+    @Test
+    public void extractPaymentsDataThrowsIOExceptionWhenFileDoesNotExist() {
+        File file = new File("A non existing file");
+
+        assertThrows(IOException.class, () -> this.validadorFichero.extractPaymentsData(file));
     }
 
 }
