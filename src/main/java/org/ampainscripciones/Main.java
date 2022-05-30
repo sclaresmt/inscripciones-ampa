f97f7c3070f7620a0b5d55c3eadfc4545c5d0f6d
Sergio <sclaresmt@gmail.com>
2022-05-30 21:33:23 +0200

    Maven packaging to make jar executable with dependencies

--- src/main/java/org/ampainscripciones/Main.java
+++ src/main/java/org/ampainscripciones/Main.java
@@ -1,9 +1,14 @@
 package org.ampainscripciones;
 
+import org.ampainscripciones.file.InscriptionsValidator;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Validando inscripciones...");
+        new InscriptionsValidator().validateAndCreateValidatedFile();
     }
 
 }
